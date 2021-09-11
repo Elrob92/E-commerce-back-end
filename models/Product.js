6dc67648902bb944341ace298e00531603a3e03b
Elrob92 <robinsonecu14@gmail.com>
2021-09-11 16:34:32 -0400

    imported four models into the database including their specific requirements

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,35 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.string,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.DECIMAL (10,2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    category_id : {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+    
   },
   {
     sequelize,
